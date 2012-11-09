cd1598eb809589adab7e2d3af268bc896c751c7d
ThomasJoyce <thomasjoyce@gmail.com>
2012-11-09 15:38:16 -0800

    Spree Version 1.3.0 - with products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MystoreSpreeEdge::Application.config.secret_token = 'a5417e534389b3590761a899145caac71de84555892c8e753d08e621efd7e8b43b9f8412b86f00f09beb760964de053168ab022e4882f1bfaa9fa9ca92cffe79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MystoreSpreeEdge::Application.config.session_store :cookie_store, key: '_mystore-spree-edge_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MystoreSpreeEdge::Application.config.session_store :active_record_store
